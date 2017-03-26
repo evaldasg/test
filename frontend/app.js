6d6eeef426d8607ae4471e2c7b472924b2d137fe
Evaldas Grudzinskas <prohdo@icloud.com>
2017-03-26 15:02:40 +0300

    index.prod

--- frontend/app.js
+++ frontend/app.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const fs = require('fs')
+const _ = require('lodash')
 const app = express()
 const port = 3001
+const baseTemplate = fs.readFileSync('./dist/index.prod.html')
+const template = _.template(baseTemplate)
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(template())
 })
 
 app.listen(port, () => {
