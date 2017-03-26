94eed2c174ccc88d7a3cf32b820a262b8919851b
Evaldas Grudzinskas <prohdo@icloud.com>
2017-03-26 08:52:55 +0300

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,6 +19,7 @@ set :passenger_restart_runner, :sequence
 set :passenger_restart_wait, 5
 set :passenger_restart_limit, 2
 set :passenger_restart_with_sudo, true
+set :passenger_restart_options, -> { "#{fetch(:deploy_to)}/current --ignore-app-not-running" }
 set :linked_files, %w(
   config/database.yml
   .env
