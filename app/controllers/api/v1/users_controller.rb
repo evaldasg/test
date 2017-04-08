68ab078bf28c81292302edecbfc2bb67efc34b9c
Evaldas Grudzinskas <prohdo@icloud.com>
2017-04-08 22:33:49 +0300

    Add jwt controller helpers

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,8 @@
 module Api
   module V1
     class UsersController < ApplicationController
+      before_action :authenticate_request!
+
       def index
         render json: User.all#[ {id: 1, name: 'User1'}, {id: 2, name: 'User2'}]
       end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1, defaults: { format: :json } do
-      resources :users, only: [:index]
+      resources :users, only: [:index, :create] do
+        collection do
+          post 'authenticate' => 'authentication#authenticate_user'
+        end
+      end
     end
   end
 end
